e792721db724c4f960accfc51be4066ccb716ea8
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-27 13:03:33 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,7 +1,7 @@
 local Translations = {
     info = {
         ["wipe_message1"]  = "Parkeer of zit in uw voertuig zo dat jouw voertuig niet word verwijderd!",
-        ["wipe_message2"]  = "Server Wipe in %{seconds} seconde(s)!!",
+        ["wipe_message2"]  = "Server Wipe in %{seconds} minuut!!",
         ["clear_message1"] = "%{count} voertuigen werden verwijderd.",
         ["clear_message2"] = "Serveropruiming voltooid, volgende wisbeurt over %{minutes} minuten!",
     }            
