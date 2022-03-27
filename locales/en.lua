7c35a497585b586e5d60f95c9ef15d964e1dc140
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-27 13:03:18 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,7 +1,7 @@
 local Translations = {
     info = {
         ["wipe_message1"]  = "Park or Sit in your vehicle to iqnore the wipe!",
-        ["wipe_message2"]  = "Server Vehicle Wipe in %{seconds} second(s)!!",
+        ["wipe_message2"]  = "Server Vehicle Wipe in %{seconds} minute!!",
         ["clear_message1"] = "%{count} vehicle are removed.",
         ["clear_message2"] = "Server Cleanup Complete, Next wipe in %{minutes} minutes",
     }            
