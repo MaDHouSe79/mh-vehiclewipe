a4e2b673f6d25c81fdcf90b7e5084042408a643b
MaDHouSe <madhouse1979@gmail.com>
2024-02-07 17:21:49 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,43 +1,32 @@
 --[[ ====================================================== ]]--
---[[    QBCore Automatic Vehicle Wipe Script by MaDHouSe    ]]--
+--[[            MH Vehicle Wipe Script by MaDHouSe          ]]--
 --[[ ====================================================== ]]--
-
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function Delete(vehicle)
     SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
     SetEntityAsMissionEntity(vehicle, false, false) 
     DeleteVehicle(vehicle)
-    if (DoesEntityExist(vehicle)) then 
-        DeleteVehicle(vehicle) 
-    end
-end
-
-local function isPlateIgnoredForWipe(plate)
-    local state = false
-    for _, v in ipairs(Config.IgnorePlates) do
-        if v == plate then state = true end
-    end
-    return state
+    if (DoesEntityExist(vehicle)) then DeleteVehicle(vehicle) end
 end
 
 RegisterNetEvent("mh-vehiclewipe:client:wipe", function()
-    QBCore.Functions.Notify(Lang:t('info.wipe_message1'), "primary", 60000)
-    QBCore.Functions.Notify(Lang:t('info.wipe_message2', {seconds = 1}), "primary", 60000)
+    QBCore.Functions.Notify(Lang:t('info.wipe_message1'), "primary", Config.WaitTimer)
+    QBCore.Functions.Notify(Lang:t('info.wipe_message2', {seconds = 1}), "primary", Config.WaitTimer)
     local count = 0
-    local ignore = 0
-    Citizen.Wait(60000)                                                                         --- Time before the cleanup is done 30 000 = 30 seconds
+    Citizen.Wait(Config.WaitTimer) --- Time before the cleanup is done 30 000 = 30 seconds
     for vehicle in EnumerateVehicles() do
         local vehicleProps = QBCore.Functions.GetVehicleProperties(vehicle)
         Wait(10)
         if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then
             QBCore.Functions.TriggerCallback("mh-vehiclewipe:server:isVehicleParked", function(parked)
                 if parked then return end
-                local ignorePlate = isPlateIgnoredForWipe(vehicleProps.plate) 
-                if ignorePlate then return end
-                Delete(vehicle)
-                count = count + 1
-                Wait(300)
+                QBCore.Functions.TriggerCallback("mh-vehiclewipe:server:isVehicleIgnored", function(ignore)
+                    if ignore then return end
+                    Delete(vehicle)
+                    count = count + 1
+                    Wait(300)
+                end, vehicleProps.plate)
             end, vehicleProps.plate)
         end
         Wait(300)
