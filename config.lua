98285e0b3e2275bfaa073facff81fe40afa947f5
MaDHouSe <madhouse1979@gmail.com>
2024-02-07 17:23:19 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,9 +5,9 @@
 Config              = {}
 Config.WaitTimer    = 60000 -- By default 1 minute
 Config.WipeTime     = 30    -- By default wipe every 30 minutes.
-Config.UseParking   = true  -- By default false, but if you are using qb-parking set this true
+Config.UseParking   = false -- By default false, but if you are using qb-parking set this true
 Config.IgnorePlates = {     -- Vehicleshop vehicle plates to be ignored for wipe.
-    [0] = "BUY-ME", 
-    [1] = "BUY ME", 
-    [2] = "TESTDRIVE",
+    "BUY-ME", 
+    "BUY ME", 
+    "TESTDRIVE",
 }
