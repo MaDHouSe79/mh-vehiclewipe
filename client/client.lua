06e1313b3399cdc012e8d0711c5fe57437bb245e
MaDHouSe <madhouse1979@gmail.com>
2024-02-15 19:05:44 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -3,34 +3,23 @@
 --[[ ====================================================== ]]--
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function Delete(vehicle)
-    SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
-    SetEntityAsMissionEntity(vehicle, false, false) 
-    DeleteVehicle(vehicle)
-    if (DoesEntityExist(vehicle)) then DeleteVehicle(vehicle) end
-end
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    TriggerServerEvent('mh-vehiclewipe:server:onjoin')
+end)
+
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        TriggerServerEvent('mh-vehiclewipe:server:onjoin')
+    end
+end)
 
 RegisterNetEvent("mh-vehiclewipe:client:wipe", function()
     QBCore.Functions.Notify(Lang:t('info.wipe_message1'), "primary", Config.WaitTimer)
     QBCore.Functions.Notify(Lang:t('info.wipe_message2', {seconds = 1}), "primary", Config.WaitTimer)
     local count = 0
-    Citizen.Wait(Config.WaitTimer) --- Time before the cleanup is done 30 000 = 30 seconds
-    for vehicle in EnumerateVehicles() do
-        local vehicleProps = QBCore.Functions.GetVehicleProperties(vehicle)
-        Wait(10)
-        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then
-            QBCore.Functions.TriggerCallback("mh-vehiclewipe:server:isVehicleParked", function(parked)
-                if parked then return end
-                QBCore.Functions.TriggerCallback("mh-vehiclewipe:server:isVehicleIgnored", function(ignore)
-                    if ignore then return end
-                    Delete(vehicle)
-                    count = count + 1
-                    Wait(300)
-                end, vehicleProps.plate)
-            end, vehicleProps.plate)
-        end
-        Wait(300)
-    end
+    Citizen.Wait(Config.WaitTimer) --- Time before the cleanup is done 30 000 = 30 seconds`
+    local coords = GetEntityCoords(PlayerPedId())
+    ClearAreaOfVehicles(coords.x, coords.y, coords.z, 10000.0, false, false, false, false, false)
     QBCore.Functions.Notify(Lang:t('info.clear_message1', {count = count}), "success", 2000)
     QBCore.Functions.Notify(Lang:t('info.clear_message2', {minutes = 30}), "success", 2000)
 end)
