8bd806cbdbc81143fc007ccf0650ecc94fe39b80
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:24:45 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ fx_version 'cerulean'
 games {'gta5'}
 author 'MaDHouSe79'
 description 'MH Automatic Vehicle Wipe, with ignore plates on wipe.'
-version '1.0'
+version '1.0.0'
 shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', 'config.lua'}
 client_scripts {'client/main.lua'}
 server_script {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
